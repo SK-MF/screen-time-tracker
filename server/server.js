b12de0dbcdec60a6573d48d35958f21b4d1a29df
SK-MF <ferminseankyle52@gmail.com>
2026-05-31 12:56:00 +0800

    fix server.js syntax error

--- server/server.js
+++ server/server.js
@@ -9,13 +9,20 @@ require("dotenv").config();
 const authRoutes = require("./routes/auth");
 
 const app = express();
+
 app.use(cors({
   origin: [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://screentime-trackers.netlify.app"  
+    "https://screentime-trackers.netlify.app"
   ]
 }));
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  family: 4,
+  serverSelectionTimeoutMS: 5000,
 })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
